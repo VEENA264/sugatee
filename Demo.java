975a83d9d7a37671deb890259643418ff2a077bc
VEENA264 <sugateveena@gmail.com>
2025-02-21 11:27:25 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public class Demo
 {
-	public static void main(String args[])
-	{
-		System.out.println("VEENA");
-	}
+   public static void main(String args[]){
+       System.out.println("hello java")
 }
+}
+
